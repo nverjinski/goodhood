bf675cdd564129e09cbf3320d0f9e4ad58b1eb7b
Nathan <nverjinski@rapidsos.com>
2025-08-22 16:46:36 -0500

    feat: added a mapUtils file and created a cubic easing function to smoothly pan the map to a new location

---
+++ src/utils/mapUtils.js
@@ -0,0 +1,53 @@
+function flyTo(map, target, duration = 2000) {
+  const safeTarget = {
+    center: target.center,
+    zoom: typeof target.zoom === "number" ? target.zoom : map.getZoom(),
+  };
+
+  const start = {
+    center: map.getCenter(),
+    zoom: map.getZoom(),
+  };
+
+  const peakZoom = 13.5;
+  const startTime = performance.now();
+
+  function easeInOutCubic(t) {
+    return t < 0.5 ? 4 * t * t * t : 1 - Math.pow(-2 * t + 2, 3) / 2;
+  }
+
+  function animate(now) {
+    const linearProgress = Math.min((now - startTime) / duration, 1);
+    const easedProgress = easeInOutCubic(linearProgress);
+    let zoom;
+
+    if (linearProgress < 0.5) {
+      // First half: Animate from start.zoom to peakZoom
+      const firstHalfProgress = linearProgress * 2;
+      const easedFirstHalf = easeInOutCubic(firstHalfProgress);
+      zoom = start.zoom + (peakZoom - start.zoom) * easedFirstHalf;
+    } else {
+      // Second half: Animate from peakZoom to safeTarget.zoom
+      const secondHalfProgress = (linearProgress - 0.5) * 2;
+      const easedSecondHalf = easeInOutCubic(secondHalfProgress);
+      zoom = peakZoom + (safeTarget.zoom - peakZoom) * easedSecondHalf;
+    }
+
+    const lat =
+      start.center.lat() +
+      (safeTarget.center.lat - start.center.lat()) * easedProgress;
+    const lng =
+      start.center.lng() +
+      (safeTarget.center.lng - start.center.lng()) * easedProgress;
+
+    map.moveCamera({ center: { lat, lng }, zoom });
+
+    if (linearProgress < 1) {
+      requestAnimationFrame(animate);
+    }
+  }
+
+  requestAnimationFrame(animate);
+}
+
+export default { flyTo };
