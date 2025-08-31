314f2cf98260970a30db99c5ff52ef77d7236c07
Nathan <nverjinski@rapidsos.com>
2025-08-30 22:45:15 -0500

    feat: added a slice to handle state for layers

---
+++ src/app/layerSlice.js
@@ -0,0 +1,26 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  layers: {
+    gunViolenceLayer: {
+      active: true,
+    },
+  },
+};
+
+export const layerSlice = createSlice({
+  name: "layer",
+  initialState,
+  reducers: {
+    toggleActiveLayer: (state, action) => {
+      const layerName = action.payload;
+      if (state.layers[layerName]) {
+        state.layers[layerName].active = !state.layers[layerName].active;
+      }
+    },
+  },
+});
+
+export const { toggleActiveLayer } = layerSlice.actions;
+
+export default layerSlice.reducer;
--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import locationReducer from "./locationSlice";
+import layerReducer from "./layerSlice";
 
 export const store = configureStore({
   reducer: {
     location: locationReducer,
+    layer: layerReducer,
   },
 });
