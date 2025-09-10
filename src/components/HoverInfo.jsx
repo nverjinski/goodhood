06dfa13bf808f00472010ef89e34bca44a3d8616
Nathan <nverjinski@rapidsos.com>
2025-09-09 20:57:27 -0500

    refactor: optimized HoverInfo component

--- src/components/HoverInfo.jsx
+++ src/components/HoverInfo.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import styled from "styled-components";
 import { LAYER_TYPES } from "@constants/layers";
 
@@ -5,10 +6,8 @@ const TooltipContainer = styled.div`
   position: absolute;
   z-index: 1;
   pointer-events: none;
-
   left: ${(props) => props.x}px;
   top: ${(props) => props.y}px;
-
   background-color: rgba(0, 0, 0, 0.8);
   color: #fff;
   padding: 4px 8px;
@@ -18,18 +17,16 @@ const TooltipContainer = styled.div`
   transform: translate(10px, -10px);
 `;
 
-const renderGunCrimeContent = (data) => {
-  return (
-    <>
-      <div>Date: {data.date}</div>
-      <div>Killed: {data.n_killed}</div>
-      <div>Injured: {data.n_injured}</div>
-      <div>Notes: {data.notes}</div>
-    </>
-  );
-};
+const GunCrimeContent = ({ data }) => (
+  <>
+    <div>Date: {data.date}</div>
+    <div>Killed: {data.n_killed}</div>
+    <div>Injured: {data.n_injured}</div>
+    <div>Notes: {data.notes}</div>
+  </>
+);
 
-const renderOffenderContent = (data) => {
+const OffenderContent = ({ data }) => {
   const fullName = [
     data.name?.givenName,
     data.name?.middleName,
@@ -38,50 +35,49 @@ const renderOffenderContent = (data) => {
     .filter(Boolean)
     .join(" ");
 
-  const residentialLocation = data.locations.filter(
-    (loc) => loc.type === "R"
-  )?.[0];
-  const streetAddress = residentialLocation.streetAddress;
-  const city = residentialLocation.city;
+  const residentialLocation = data.locations?.find((loc) => loc.type === "R");
 
   return (
     <>
       <div>Name: {fullName}</div>
       <div>Age: {data.age}</div>
-      <div>Street: {streetAddress}</div>
-      <div>City: {city}</div>
+      <div>Street: {residentialLocation?.streetAddress}</div>
+      <div>City: {residentialLocation?.city}</div>
       <div>Jurisdiction: {data.jurisdictionId}</div>
       <div>Absconder: {data.absconder ? "Yes" : "No"}</div>
     </>
   );
 };
 
-const renderContent = (type, data) => {
-  switch (type) {
-    case LAYER_TYPES.GUN_CRIME:
-      return renderGunCrimeContent(data);
+const HoverInfo = React.memo(({ hoverInfo }) => {
+  const { object, x, y } = hoverInfo || {};
 
-    case LAYER_TYPES.OFFENDER:
-      return renderOffenderContent(data);
-
-    default:
-      return null;
-  }
-};
-
-const HoverInfo = ({ hoverInfo }) => {
-  if (!hoverInfo || !hoverInfo.object) {
+  if (!object) {
     return null;
   }
+
   const type = hoverInfo?.object?.incident_id
     ? LAYER_TYPES.GUN_CRIME
     : LAYER_TYPES.OFFENDER;
 
+  const renderContent = () => {
+    switch (type) {
+      case LAYER_TYPES.GUN_CRIME:
+        return <GunCrimeContent data={object} />;
+
+      case LAYER_TYPES.OFFENDER:
+        return <OffenderContent data={object} />;
+
+      default:
+        return null;
+    }
+  };
+
   return (
-    <TooltipContainer x={hoverInfo.x} y={hoverInfo.y}>
-      {renderContent(type, hoverInfo.object)}
+    <TooltipContainer x={x} y={y}>
+      {renderContent()}
     </TooltipContainer>
   );
-};
+});
 
 export default HoverInfo;
