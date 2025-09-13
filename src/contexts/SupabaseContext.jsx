1ddd0a07b88796cc9e69f1fa6243c80c4cda8782
Nathan <nverjinski@rapidsos.com>
2025-09-13 11:22:32 -0500

    feat: installed Supabase and created a react context to hold the client instance

---
+++ src/contexts/SupabaseContext.jsx
@@ -0,0 +1,16 @@
+import { createContext } from "react";
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseURL = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
+export const supabase = createClient(supabaseURL, supabaseAnonKey);
+const SupabaseContext = createContext(supabase);
+
+export const SupabaseProvider = ({ children }) => {
+  return (
+    <SupabaseContext.Provider value={supabase}>
+      {children}
+    </SupabaseContext.Provider>
+  );
+};
