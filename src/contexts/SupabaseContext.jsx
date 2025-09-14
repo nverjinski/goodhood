a3b0e78fd8f4bb99827edaba9b8e96bbc4e13c11
Nathan <nverjinski@rapidsos.com>
2025-09-13 21:25:07 -0500

    chore: updated supabase client to use the api schema

--- src/contexts/SupabaseContext.jsx
+++ src/contexts/SupabaseContext.jsx
@@ -4,8 +4,12 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseURL = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-export const supabase = createClient(supabaseURL, supabaseAnonKey);
-const SupabaseContext = createContext(supabase);
+export const supabase = createClient(supabaseURL, supabaseAnonKey, {
+  db: {
+    schema: "api",
+  },
+});
+export const SupabaseContext = createContext(supabase);
 
 export const SupabaseProvider = ({ children }) => {
   return (
